ba0908fb97dc8d469e7bc96bb097bd7f90d10867
Prateek Rana <52359250+ImTheHappyFace@users.noreply.github.com>
2022-03-04 02:56:03 +0530

    4th march

---
+++ assets/js/larq.js
@@ -0,0 +1,34 @@
+feather.replace()
+
+// document.getElementById('ham_menu').
+//     addEventListener('click',function(){
+//         document.getElementById('side_icon').style.display = "block"
+//     })
+
+
+
+// Menu Show
+
+function menuShow(){
+    let count =0
+
+    document.getElementById('hidden_menu').
+    classList.add("show")
+    document.getElementById('hidden_menu').
+    classList.remove("hide")
+    document.getElementById("plus").style.display= "none"
+    document.getElementById("minus").style.display= "inline-flex"
+
+}
+
+function menuHide(){
+    let count =0
+
+    document.getElementById('hidden_menu').
+    classList.add("hide")
+    document.getElementById("plus").style.display= "inline-flex"
+    document.getElementById("minus").style.display= "none"
+
+    console.log("hellos");
+}
+
